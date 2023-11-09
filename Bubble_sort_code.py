bf6749285b78c9d8525c91e4ebfc0396fe0b1013
Naik999 <149890250+Naik999@users.noreply.github.com>
2023-11-09 14:44:00 +0530

    Update Bubble_sort_code.py

--- Bubble_sort_code.py
+++ Bubble_sort_code.py
@@ -1,11 +1,12 @@
-def number():
-    num = (input("enter:"))
-    num = list(num)
-    length_of = len(num)
+num_a = input("give values with space: ")
+num_a = [int(x) for x in  num_a.split()]
+
+def bubble_number(a):  
+    length_of = len(a)
     for i in range(length_of):
-         for j in range(length_of):
-                if num[i] < num[j]:
-                    num[i],num[j] = num[j],num[i]
-    return num
-sorted_num = number()
-print(sorted_num)
+        for j in range(0,length_of-i-1):
+            if a[j] > a[j+1]:
+                a[j],a[j+1] = a[j+1],a[j]
+    return a
+sorted_number = bubble_number(num_a)
+print(sorted_number)
