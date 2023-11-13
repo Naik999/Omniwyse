9b75e1a281866d6358485502a23d30c38d22afb6
Naik999 <149890250+Naik999@users.noreply.github.com>
2023-11-13 17:55:08 +0530

    Add files via upload

---
+++ extrenal module .py
@@ -0,0 +1,8 @@
+import pyfiglet
+from termcolor import colored
+
+name = input("enter your name")
+color =input("what color do you want")
+art = pyfiglet.figlet_format("name")
+art_color = colored(art,color = color)
+print(art_color)
