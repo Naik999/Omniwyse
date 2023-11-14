f94aec2c907cf991de315479b65915a153507e42
Naik999 <149890250+Naik999@users.noreply.github.com>
2023-11-14 12:33:02 +0530

    Update extrenal module .py

--- extrenal module .py
+++ extrenal module .py
@@ -3,6 +3,6 @@ from termcolor import colored
 
 name = input("enter your name")
 color =input("what color do you want")
-art = pyfiglet.figlet_format("name")
-art_color = colored(art,color = color)
+art = pyfiglet.figlet_format(name)
+art_color = colored(art,color)
 print(art_color)
