0bf33df63b7ea37bbf37ab5657058653137de6dd
Naik999 <149890250+Naik999@users.noreply.github.com>
2023-11-15 12:27:18 +0530

    Update Assignment-5_json.py

--- Assignment-5_json.py
+++ Assignment-5_json.py
@@ -2,10 +2,15 @@ import json
 
 with open("dict.json", "r") as data_son:
     data = json.load(data_son)
+dict = {}
 for student in data['student']:
-    if student ['m_Name'] != "null":
-        print(f"{student['id']}.{student['f_Name']}{student['m_Name']}{student['l_Name']}@gmail.com")
-    else : 
-        print(f"{student['id']}.{student['f_Name']}{student['l_Name']}@gmail.com")
-    details = json.dumps(student,indent=4)
-    print(details)
+
+    if student['m_Name'] != "null":
+        fn = student['f_Name']+student['m_Name']+student['l_Name']
+        email =f"{student ['collage']}.{student['id']}{student['f_Name']}{student['m_Name']}{student['l_Name']}@gmail.com"
+    else:
+        fn =student['f_Name']+student['l_Name']
+        email =f"{student ['collage']}.{student['id']}.{student['f_Name']}{student['l_Name']}@gmail.com"
+    dict[email]={"id": student["id"],"fullname": fn}
+    details = json.dumps(dict,indent=(3))   
+print(details)
