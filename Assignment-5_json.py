74fea685aa8e27827610160e0d62e8c5eb022d62
Naik999 <149890250+Naik999@users.noreply.github.com>
2023-11-16 22:38:55 +0530

    Update Assignment-5_json.py

--- Assignment-5_json.py
+++ Assignment-5_json.py
@@ -2,7 +2,7 @@ import json
 
 with open("dict.json", "r") as data_son:
     data = json.load(data_son)
-dict = {}
+new_dict = {}
 for student in data['student']:
 
     if student['m_Name'] != "null":
@@ -11,6 +11,8 @@ for student in data['student']:
     else:
         fn =student['f_Name']+student['l_Name']
         email =f"{student ['collage']}.{student['id']}.{student['f_Name']}{student['l_Name']}@gmail.com"
-    dict[email]={"id": student["id"],"fullname": fn}
-details = json.dumps(dict,indent=(3))   
-print(details)
+    new_dict[email]={"id": student["id"],"fullname": fn}
+    
+with open('change_data.json',"w") as file_data:
+    json.dump(new_dict,file_data,indent=(3))
+print(file_data)
